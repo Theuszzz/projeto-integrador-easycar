ba64d6d05754b39593424815e3e8e84212c1ef41
Theuszzz <matheus.fernandes.r10@gmail.com>
2025-12-10 00:31:59 -0300

    Resolvido Conflito Got - feat: pesquisar alugados

--- cars/views.py
+++ cars/views.py
@@ -9,14 +9,22 @@ from users.permissions import IsFuncionarioOuSuperuser
 from rest_framework.permissions import IsAuthenticated
 
 
-class CarroViewSet(viewsets.ModelViewSet):
-    serializer_class = CarroSerializer
+class CarroViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Carro.objects.all()
     permission_classes = [IsAuthenticated, IsFuncionarioOuSuperuser]
+    serializer_class = CarroSerializer
 
-    # Rota personalizada: /api/carros/disponiveis/
-    @action(detail=False, methods=['get'], url_path='disponiveis')
+    # Rota: /api/carros/disponiveis/
+    @action(detail=False, methods=['get'])
     def disponiveis(self, request):
         carros = self.queryset.filter(status='disponivel')
         serializer = self.get_serializer(carros, many=True)
         return Response(serializer.data)
+
+    # Rota: /api/carros/alugados/
+    @action(detail=False, methods=['get'])
+    def alugados(self, request):
+        carros = self.queryset.filter(status='alugado')
+        serializer = self.get_serializer(carros, many=True)
+        return Response(serializer.data)
+

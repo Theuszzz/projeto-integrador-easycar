9df785e9239940bc0a44bd5669997980a5f129bb
Maria Luiza <marialluantunes@gmail.com>
2025-12-12 01:03:28 -0300

    feat(users): adiciona primeiro_nome obrigatório e campos is_staff/is_superuser somente leitura

--- users/serializers.py
+++ users/serializers.py
@@ -6,19 +6,31 @@ from .models import PerfilCliente
 
 class UserSerializer(serializers.ModelSerializer):
     senha = serializers.CharField(write_only=True)
+    primeiro_nome = serializers.CharField(source='first_name', required=True)  # mapeia first_name, campo obrigatorio
+    is_staff = serializers.BooleanField(default=False, read_only=True)
+    is_superuser = serializers.BooleanField(default=False, read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'senha', 'is_active']
+        fields = ['id', 'username', 'email', 'primeiro_nome' , 'is_staff' , 'senha', 'is_active' , 'is_superuser']
 
     def create(self, validated_data):
+
+        first_name = validated_data.pop('first_name') # pega e remove o primeiro nome do dicionario
+        senha = validated_data.pop('senha') # pega e remove senha do dicionario
+
+
         # cria usuarios
         cliente = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=validated_data['senha'],
-            is_active=True
+            first_name=first_name,
+            password=senha,
+            is_active=True,
+            is_staff=False,
+            is_superuser=False
         )
+        
 
         # adiciona automaticamente ao grupo "Clientes"
         grupo_cliente = Group.objects.get(name="Clientes")
